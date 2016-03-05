2d384d52b57f204beb420df302a6a53c703c0d12
pvillaul <p.villaluca@gmail.com>
2016-03-05 12:46:10 +0100

    prueba de cambios en mi rama

--- Maquetacion e-AytoMAD/js/agency.js
+++ Maquetacion e-AytoMAD/js/agency.js
@@ -24,3 +24,17 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function() {
+        $("#content div").hide();
+        $("#tabs li:first").attr("id","current");
+        $("#content div:first").fadeIn();
+ 
+    $('#tabs a').click(function(e) {
+        e.preventDefault();
+        $("#content div").hide();
+        $("#tabs li").attr("id","");
+        $(this).parent().attr("id","current");
+        $('#' + $(this).attr('title')).fadeIn();
+    });
+})();
