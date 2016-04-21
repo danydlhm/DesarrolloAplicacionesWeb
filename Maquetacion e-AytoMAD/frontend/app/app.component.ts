e42374d06ec7e13e7734ac31b2bc95caf5f5cf06
Daniel de las Heras Montero <Dany@MacBook-Air-de-Daniel.local>
2016-04-21 20:26:05 +0200

    Loggin y registro

--- Maquetacion e-AytoMAD/frontend/app/app.component.ts
+++ Maquetacion e-AytoMAD/frontend/app/app.component.ts
@@ -2,15 +2,19 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {IndexComponent} from './index.component';
 import {CiudadanoComponent} from './ciudadano.component';
-import {FooterComponent} from './footer.component'
+import {FooterComponent} from './footer.component';
+import {LogginComponent} from './loggin.component';
+import {SignUpComponent} from './signUp.component';
 
 @Component({
   selector: 'app',
   templateUrl: 'app/appTemplate.html',
-  directives: [ROUTER_DIRECTIVES, FooterComponent]
+  directives: [ROUTER_DIRECTIVES, FooterComponent],
 })
 @RouteConfig([
   {path: '/index', name: 'Index', component: IndexComponent, useAsDefault: true},
-  {path: '/usuario/concejal', name: 'Ciudadano', component: CiudadanoComponent},
+  {path: '/usuario/concejal', name: 'Ciudadano', component: CiudadanoComponent},  
+  {path: '/loggin', name: 'Loggin', component: LogginComponent}, 
+  {path: '/signup', name: 'SignUp', component: SignUpComponent}, 
 ])
 export class AppComponent { }
