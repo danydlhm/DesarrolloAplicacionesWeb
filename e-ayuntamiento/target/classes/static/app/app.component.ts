08752c0f9f9e4016f419f9f9f1a62611fec79723
Daniel de las Heras <d.delasherasm@alumnos.urjc.es>
2016-05-17 19:06:56 +0200

    modificado pom

--- e-ayuntamiento/target/classes/static/app/app.component.ts
+++ e-ayuntamiento/target/classes/static/app/app.component.ts
@@ -1,28 +1,40 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {HTTP_PROVIDERS, Http} from 'angular2/http';
-
-import {BookListComponent} from './book-list.component';
-import {BookDetailComponent} from './book-detail.component';
-import {BookFormComponent} from './book-form.component';
-import {LoginComponent} from './login.component';
-
-import {BookService} from './book.service';
-import {LoginService} from './login.service';
-
-import {Alert} from 'ng2-bootstrap/ng2-bootstrap';
+import {FooterComponent} from './footer.component';
+import {IndexComponent} from './index/index.component';
+import {LogginComponent} from './index/loggin.component';
+import {SignUpComponent} from './index/signUp.component';
+import {CiudadanoComponent} from './usuarios/ciudadano.component';
+import {PropuestaService} from './usuarios/propuesta.service';
+import {AdministradorComponent} from './usuarios/administrador.component';
+import {SpinnerComponent} from './spinner.component';
+import {ConcejalService} from './index/concejal.service';
+import {CarrouselService} from './index/carrousel.service';
+import {ActaListComponent} from './index/acta-list.component';
+import {ActaDetailComponent} from './index/acta-detail.component';
+import {ActaService} from './index/acta.service';
+import {NoticiaDetailComponent} from './index/noticia-detail.component';
+import {NoticiaService} from './index/noticia.service';
+import {NoticiaListComponent} from './index/noticia-list.component';
 
 @Component({
   selector: 'app',
-  templateUrl: 'app/app.component.html',
-  providers:  [BookService, LoginService, HTTP_PROVIDERS],
-  directives: [LoginComponent, ROUTER_DIRECTIVES, Alert]
+  templateUrl: 'app/appTemplate.html',
+  providers:  [ConcejalService,CarrouselService,ActaService, NoticiaService, PropuestaService],
+  directives: [ROUTER_DIRECTIVES, FooterComponent, SpinnerComponent]
 })
 @RouteConfig([
-  {path: '/books', name: 'Books', component: BookListComponent, useAsDefault: true},
-  {path: '/book/:id', name: 'BookDetail', component: BookDetailComponent},
-  {path: '/book/new', name: 'BookNew', component: BookFormComponent},
-  {path: '/book/edit/:id', name: 'BookEdit', component: BookFormComponent}
+  {path: '/index', name: 'Index', component: IndexComponent, useAsDefault: true},
+  {path: '/usuario/concejal/...', name: 'Ciudadano', component: CiudadanoComponent},
+  /**{path: '/usuario/concejal/propuesta/:id', name: 'PropuestaDetail', component: PropuestaDetailComponent},
+  {path: '/usuario/concejal/propuestas/', name: 'PropuestaList', component: PropuestaListComponent},
+  {path: '/usuario/concejal/propuestas/nueva/', name: 'PropuestaNew', component: PropuestaFormComponent},*/
+  {path: '/usuario/administrador', name: 'Administrador', component: AdministradorComponent},
+  {path: '/usuario/administrador/noticias', name: 'AdminNews', component: NoticiaListComponent},
+  {path: '/loggin', name: 'Loggin', component: LogginComponent}, 
+  {path: '/signup', name: 'SignUp', component: SignUpComponent},
+  {path: '/index/noticia/:id', name: 'NoticiaDetail', component: NoticiaDetailComponent},
+  {path: '/index/acta/:id', name: 'ActaDetail', component: ActaDetailComponent},
+  {path: '/index/actas/', name: 'ActaList', component: ActaListComponent},
 ])
-export class AppComponent {	
-}
+export class AppComponent { }
