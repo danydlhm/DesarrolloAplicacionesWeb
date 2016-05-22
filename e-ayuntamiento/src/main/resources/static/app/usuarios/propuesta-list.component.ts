66fb03bb0a976143e25012bad710df53207fb0bb
pvillaul <p.villaluca@gmail.com>
2016-05-22 20:32:06 +0200

    Añadido estilo a las propuestas

--- e-ayuntamiento/src/main/resources/static/app/usuarios/propuesta-list.component.ts
+++ e-ayuntamiento/src/main/resources/static/app/usuarios/propuesta-list.component.ts
@@ -7,20 +7,32 @@ import {Propuesta, PropuestaService}   from './propuesta.service';
     template: `
     <div class="row">
         <div class="col-md-4 col-sm-6 portfolio-item text-center" *ngFor="#propuesta of propuestas">
+            <div class="cajita">
             <a [routerLink]="['PropuestaDetail', {id: propuesta.id}]" class="portfolio-link">
-                <img src="{{propuesta.imagen}}" class="img-responsive center-block" alt="foto descriptiva de la propuesta">
+                <hr>
+                <img src="{{propuesta.imagen}}" class="img-responsive img-rounded center-block" alt="foto descriptiva de la propuesta" height="350" width="350">
+                <hr>
                 <div class="portfolio-caption">
                     <h5>Propuesta nº{{propuesta.id}} :</h5>
                     <h6>{{propuesta.titulo}}</h6>
                 </div>
             </a>
+            </div>
         </div>
     </div>
     <p class="text-center">
         <button class="btn btn-primary" (click)="newPropuesta()">Nueva Propuesta</button>
         <button class="btn btn-primary" (click)="gotoIndex()">Atrás</button>
-    </p>
-  `
+    </p>`,
+    styles: [`
+        .cajita{
+            border-radius: 50px 10px 50px 10px;
+            -moz-border-radius: 50px 10px 50px 10px;
+            -webkit-border-radius: 50px 10px 50px 10px;
+            border: 1.5px solid #191919;
+            background: #d9f5f4;
+        }
+    `]
 })
 export class PropuestaListComponent implements OnInit {
 
